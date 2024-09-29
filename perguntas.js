a9ba6c693d1001f6610c11ab8855cf0d81703e27
Pedro Henrique Amaral da Silva <148217292+pedrohenriqueeee@users.noreply.github.com>
2024-09-29 13:49:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao (
+    'Pantera Negra',
+    'Qual é a principal fonte de riqueza de Wakanda?',
+    'Vibranium'
+)
+criaCartao (
+    'Pantera Negra',
+   'Quem é o vilão principal no filme "Pantera Negra"?',
+    'Erik Killmonger'
+)
+
+criaCartao (
+    'Pantera Negra',
+   'Qual é o nome da irmã de T’Challa?',
+    'Shuri'
+)
+criaCartao (
+    'Pantera Negra',
+   'Quem é o ator que interpretou o Pantera Negra nos filmes do Marvel Cinematic Universe?',
+    'Chadwick Boseman'
+)
+
